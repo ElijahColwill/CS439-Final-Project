2cfb6805ccfa05022b8f4336c19e7ef7c3b82daa
dMahra <dmahra@purdue.edu>
2022-11-10 13:35:01 -0500

    started map.py implementation

--- map.py
+++ map.py
@@ -1,4 +1,6 @@
-import geopandas
+import geopandas as gpd
+from geopandas import GeoDataFrame
+from shapely import wkt
 from shapely.geometry import LineString
 import matplotlib.pyplot as plt
 from matplotlib import axes
@@ -7,3 +9,21 @@ import pandas as pd
 
 def plot(data: pd.DataFrame, ax: axes.Axes, attribute: str, secondary: str):
     print(f'Called Map: {attribute}, {secondary}')
+    # print(data.head())
+    # print(type(data.iloc[0]['state_boundary']))
+    data['state_boundary'] = gpd.GeoSeries.from_wkt(data['state_boundary'])
+    state_boundaries = GeoDataFrame(data, geometry=data['state_boundary'])
+    data['county_boundary'] = gpd.GeoSeries.from_wkt(data['county_boundary'])
+    county_boundaries = GeoDataFrame(data, geometry=data['county_boundary'])
+    state_boundaries.plot(ax=ax, edgecolor='k', color='lightgray', zorder=1)
+    # county_boundaries.plot(x=ax, edgecolor='k', color='lightgray', zorder=2)
+
+    ax.set_aspect('auto')
+    plt.ylim([23, 51])
+    plt.xlim([-125, -65])
+
+
+    # fig, ax = plt.subplots()
+    # fig.set_size_inches(12, 5)
+    # fig.set_dpi(125)
+    # plt.show()
--- stream_graph.py
+++ stream_graph.py
@@ -4,3 +4,4 @@ import pandas as pd
 
 def plot(community_data: pd.DataFrame, hesitancy_data: pd.DataFrame, ax: axes.Axes):
     print('Called Stream Graph')
+
